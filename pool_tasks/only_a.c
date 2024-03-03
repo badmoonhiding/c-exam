c7d397b9e23adedc531fd016c9528d7ce080912e
Alina Ananieva <bwldthesis@gmail.com>
2024-03-03 00:32:13 +0000

    small changes

--- pool_tasks/only_a.c
+++ pool_tasks/only_a.c
@@ -1,9 +1,18 @@
 #include <unistd.h>
+int only_a();
 
 int    main(void)
+{
+    only_a();
+    return (0);
+}
+
+int    only_a(void)
 {
     char c;
     c = 'a';
     write(1, &c, 1);
     return (0);
 }
+
+
